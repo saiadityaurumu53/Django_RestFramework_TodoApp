19dd402229433b00a0aae5eb371cb8ceab811038
Sai Aditya Urumu <saiadityaurumu53@gmail.com>
2024-06-05 00:49:35 -0500

    (1) Created the List view api for the Django Todo project

---
+++ todo/todo_api/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+"""
+In this section, we’ll walk through how to create two API views: 
+1) list view
+2) detail view
+
+"""
+
+
+class TodoListApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 1. List all
+    def get(self, request, *args, **kwargs):
+        '''
+        List all the todo items for given requested user
+        '''
+        todos = Todo.objects.filter(user = request.user.id)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 2. Create
+    def post(self, request, *args, **kwargs):
+        '''
+        Create the Todo with given todo data
+        '''
+        data = {
+            'task': request.data.get('task'), 
+            'completed': request.data.get('completed'), 
+            'user': request.user.id
+        }
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
